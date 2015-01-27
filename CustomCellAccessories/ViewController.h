eaf5ddda4ce286de3551b8430564c8ea4882ded1
pradip <pradip@pradips-Mac.local>
2015-01-27 14:59:10 +0530

    Initial Commit

---
+++ CustomCellAccessories/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CustomCellAccessories
+//
+//  Created by pradip on 1/27/15.
+//  Copyright (c) 2015 pradip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
