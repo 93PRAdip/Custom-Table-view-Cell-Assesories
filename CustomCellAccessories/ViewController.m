350d823966ce7a3ced35cafec60bf7b728b60139
pradip <pradip@pradips-Mac.local>
2015-01-27 15:49:00 +0530

    first commit

--- CustomCellAccessories/ViewController.m
+++ CustomCellAccessories/ViewController.m
@@ -7,9 +7,9 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+static NSString *TableViewCellIdentifier= @"MyCell";
+@interface ViewController ()<UITableViewDataSource>
+@property(nonatomic,strong) UITableView *myTableView;
 @end
 
 @implementation ViewController
@@ -17,7 +17,62 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.myTableView =[[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    [self.myTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:TableViewCellIdentifier];
+    self.myTableView.dataSource=self;
+    self.myTableView.autoresizingMask=UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+    [self.view addSubview:self.myTableView];
+	
+}
+//option for tag a cell
+-(UIView *)superViewOfType:(Class)paramSuperViewClass forView:(UIView *)paramView
+{
+    if(paramView.subviews !=nil)
+    {
+        if([paramView.superview isKindOfClass:paramSuperViewClass])
+        {
+            return paramView.superview;
+            
+        }
+        else{
+            return [self  superViewOfType:paramSuperViewClass forView:paramView.superview];
+        }
+    }
+    return nil;
+}
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell  *cell=nil;
+    cell=[tableView dequeueReusableCellWithIdentifier:TableViewCellIdentifier forIndexPath:indexPath];
+    cell.textLabel.text=[NSString stringWithFormat:@"section %ld ,Cell %ld",(long)indexPath.section,(long)indexPath.row];
+   //cell.accessoryType=UITableViewCellAccessoryDetailDisclosureButton;
+    UIButton *button =[UIButton buttonWithType:UIButtonTypeSystem];
+    button.frame=CGRectMake(0, 0, 15, 25);
+    [button setTitle:@"Expand" forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(performedExpand:) forControlEvents:UIControlEventTouchUpInside];
+    cell.accessoryView=button;
+
+    
+    return cell;
+}
+
+
+- (void) performExpand:(UIButton *)paramSender{
+    
+    /* Handle the tap event of the button */
+    __unused UITableViewCell *parentCell =(UITableViewCell *)[self superViewOfType:[UITableViewCell class] forView:paramSender];
+    
+    /* Now do something with the cell if you want to */
+    
 }
 
 - (void)didReceiveMemoryWarning
